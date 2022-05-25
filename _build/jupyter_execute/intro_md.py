7d2ad5423ad8071919463032ff85981e6f231d90
stefy-naranjo <stefy_naranjo@hotmail.com>
2022-05-24 20:21:52 -0500

    Archivo Nuevo 2.0

---
+++ _build/jupyter_execute/intro_md.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # PRESENTACION PERSONAL
+# 
+#  ![title](img/stefy.png)
+# 
+# ## NOMBRE: STEFANNY NARANJO            
+# 
+# ## CEDULA: 1805182001
+# 
+# ## SEXTO SEMESTRE DE TECNOLOGIAS DE LA INFORMACION 
+# 
+# Soy Stefanny del Rosario Naranjo Casco tengo 28 anios vivo enla coiudad de Banios de Agua Santa de a provincia de Tungurahua, me gusta la programacion, con anterioridad tengo un tecnologado en analisis de sistemas,  tengo conocimientos en C++, php y Javascript.
+# 
+# 
+# ### Aspiraciones
+# 
+# Actualmente mis deseos son culminar la carrera para posteriormente postular a una beca internacional y asi lograr obtener una maestria en Inteligencia Artificial y posterior a eso ejercer un vida laboral basada en mis valores y etica, ya sea en el pais o en el exterior.
+# 
+# Mi suenio es especializarme en el pais de Corea del Sur ya que a mi criterio es no de los pioneros en cuanto a los avances tecnologicos se tratan.
+# 
+
+# Este proyecto esta basado en 3 paginas las cuaes enlistare a continuacion:
+#  - Histrial Academico
+#  - Avance de Titulacion
+#  - Corea del Sur
